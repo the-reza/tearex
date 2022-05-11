848a286ec69ba70e575fbef0256e86a331cd35e8
the-reza <r_torkashvand@yahoo.com>
2022-05-11 18:18:58 +0430

    enter

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -13,7 +13,7 @@ function About() {
                         <div className="main-about">
                               <h1 className='h1-about'>ABOUT ME</h1>
                               <p>
-                                    - I'm Reza, an smart and kind guy.<br /> - I was Born on December 18, 2005 in Iran.<br />- Like to Program and Code.<br />
+                                    - I'm Reza, a smart and kind guy.<br /> - I was Born on December 18, 2005 in Iran.<br />- Like to Program and Code.<br />
                                     - A programmer since 2021
                                     <h2 className='about-t'>Contact Me: tearex05@yahoo.com</h2>
                               </p>
--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -3,15 +3,18 @@ import Nav from '../navbar/Nav'
 import Example from './Example'
 import './style.css'
 import { motion } from 'framer-motion'
+import Tour from './example pics/tour-main-page.PNG';
+import Hamburger from "./example pics/hamburger.png"
+import Benz from './example pics/benz.PNG'
 
 function Works() {
       return (
             <>
                   <Nav />
                   <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className='body-works'>
-                        <Example link='#' img='https://images.unsplash.com/photo-1617791160588-241658c0f566?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80' />
-                        <Example link='#' img='https://images.unsplash.com/photo-1618005198919-d3d4b5a92ead?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80' />
-                        <Example link='#' img='https://images.unsplash.com/photo-1622547748225-3fc4abd2cca0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80' />
+                        <Example link='https://the-reza.github.io/tour/' img={Tour} />
+                        <Example link='https://the-reza.github.io/hamburger/' img={Hamburger} />
+                        <Example link='https://the-reza.github.io/benz' img={Benz} />
                         <Example link='#' img='https://images.unsplash.com/photo-1632059368252-be6d65abc4e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80' />
                         <Example link='#' img='https://images.unsplash.com/photo-1617396900799-f4ec2b43c7ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80' />
                         <Example link='#' img='https://images.unsplash.com/photo-1617791160588-241658c0f566?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80' />
